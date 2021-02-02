03d941b94b3cfdb4f4423787df11cc52e0df5318
Neuza Sousa <neuzarssousa@gmail.com>
2021-02-02 18:28:22 +0100

    Added country flags

--- src/LanguageSelector.js
+++ src/LanguageSelector.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import EnglishImg from './images/gb.png';
+import GermanImg from './images/de.png';
+import FrenchImg from './images/fr.png';
+import PortugueseImg from './images/pt.png';
 
 const LanguageSelector = () => {
   const { t, i18n } = useTranslation()
@@ -10,10 +14,10 @@ const LanguageSelector = () => {
 
   return (
     <div onChange={changeLanguage}>
-      <input type="radio" value="en" name="language" defaultChecked /> English
-      <input type="radio" value="de" name="language" /> German
-      <input type="radio" value="fr" name="language" /> French
-      <input type="radio" value="pt" name="language"/> Portuguese
+      <input type="radio" value="en" name="language" defaultChecked /> English <img src={EnglishImg} alt="English" />
+      <input type="radio" value="de" name="language" /> German <img src={GermanImg} alt="German" />
+      <input type="radio" value="fr" name="language" /> French <img src={FrenchImg} alt="French" />
+      <input type="radio" value="pt" name="language"/> Portuguese <img src={PortugueseImg} alt="Portuguese" />
     </div>
   )
 }
